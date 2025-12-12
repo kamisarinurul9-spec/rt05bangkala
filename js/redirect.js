cf83323d78e82f3f02982cb605c3cc48aea98fd7
kamisarinurul9-spec <kamisarinurul9@gmail.com>
2025-12-12 09:33:53 +0700

    Update redirect.js

--- js/redirect.js
+++ js/redirect.js
@@ -1,17 +1,12 @@
-// Halaman yang tidak butuh login
-const publicPages = ["login.html", "index.html"];
+// Lindungi semua halaman kecuali login
+const allowedPublic = ["login.html", "index.html"];
 
-// Ambil halaman yang sedang dibuka
 const currentPage = window.location.pathname.split("/").pop();
 
-// Jika halaman tidak ada di folder (GitHub Pages), fallback index
-const safePage = currentPage === "" ? "index.html" : currentPage;
-
-// Jika halaman bukan public → wajib login
-if (!publicPages.includes(safePage)) {
-    const logged = localStorage.getItem("rt05_logged");
-
-    if (logged !== "true") {
+if (!allowedPublic.includes(currentPage)) {
+    const user = JSON.parse(localStorage.getItem("user"));
+    
+    if (!user) {
         window.location.href = "login.html";
     }
 }
