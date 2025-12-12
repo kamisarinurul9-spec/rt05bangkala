d48f518d8b7c62eae78cfa9e7735e0020c373dd9
kamisarinurul9-spec <kamisarinurul9@gmail.com>
2025-12-12 16:33:55 +0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,14 +1,12 @@
-// =============================
+// ==================================
 // AUTH SYSTEM FINAL VERSION
-// =============================
+// ==================================
 
-// Demo akun
 const users = [
   { username: "admin", password: "admin123", role: "admin" },
   { username: "warga", password: "warga123", role: "warga" }
 ];
 
-// LOGIN
 function login(event) {
   event.preventDefault();
 
@@ -18,14 +16,12 @@ function login(event) {
   const found = users.find(x => x.username === u && x.password === p);
 
   if (!found) {
-    alert("Username atau Password salah!");
+    alert("Username atau password salah!");
     return;
   }
 
-  // simpan session
   localStorage.setItem("rt05_user", JSON.stringify(found));
 
-  // redirect sesuai role
   if (found.role === "admin") {
     window.location.href = "admin-dashboard.html";
   } else {
@@ -33,22 +29,22 @@ function login(event) {
   }
 }
 
-// CHECK AUTH PER PAGE
 function checkAuth(requiredRole = null) {
-  const user = JSON.parse(localStorage.getItem("rt05_user"));
+  const userRaw = localStorage.getItem("rt05_user");
 
-  if (!user) {
+  if (!userRaw) {
     window.location.href = "login.html";
     return;
   }
 
+  const user = JSON.parse(userRaw);
+
   if (requiredRole && user.role !== requiredRole) {
-    alert("Tidak memiliki akses halaman ini!");
+    alert("Anda tidak punya akses halaman!");
     window.location.href = "login.html";
   }
 }
 
-// LOGOUT
 function logout() {
   localStorage.removeItem("rt05_user");
   window.location.href = "login.html";
