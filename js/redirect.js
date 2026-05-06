a6fd0e3e55d49e04779937694949f3b043272ec7
kamisarinurul9-spec <kamisarinurul9@gmail.com>
2026-05-06 22:52:52 +0700

    Delete js directory

--- js/redirect.js
+++
@@ -1,7 +0,0 @@
-// redirect.js
-const publicPages = ["login.html","index.html"];
-const current = window.location.pathname.split("/").pop();
-if(!publicPages.includes(current)){
-  const user = JSON.parse(localStorage.getItem("user"));
-  if(!user){ window.location.href="login.html"; }
-}
